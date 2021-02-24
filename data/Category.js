924be00e7ffdc00404750284089e4cdfa6b6898e
Mateusz Chojnowski <mateusz.w.chojnowski@gmail.com>
2021-02-23 19:27:40 -0500

    data

--- data/Category.js
+++ data/Category.js
@@ -5,7 +5,7 @@ export const AllProducts = [
     description:
       "Upgrade your bedroom with the sophisticated look of this chic comforter set from The Curated Nomad. Made of soft cotton, this reversible comforter set features a solid color back and a top embellished with geometric prints and tufted chenille accents that add dimension to the overall design. Two matching pillow shams complete this set's beautiful look.",
     brand: "The Curated Nomad",
-    rating: 4.1,
+    rating: 4,
     price: 95.98,
     sizes: ["King", "Queen"],
     imgNum: 3,
@@ -26,7 +26,7 @@ export const AllProducts = [
     description:
       "RELAXING COMFORT: After a long day, you need to unwind. So, go ahead. Wrap yourself up with Bare Home’s cotton weighted blanket. Designed to mimic the sensation of being held with calming breathable pressure. Our blanket provides sensational comfort to help achieve the most relaxing night of sleep. NEW & IMPROVED DESIGN: Get the sleep you’ve been missing! Our redesigned blanket includes smaller pockets, reinforced stitching and increased filling to keep your beads from shifting and leaking.",
     brand: "Osleep",
-    rating: 4.6,
+    rating: 4,
     price: 54.32,
     sizes: ["40x60-10lb", "40x60-7lb"],
     imgNum: 2,
@@ -45,7 +45,7 @@ export const AllProducts = [
     description:
       "This mattress minimizes motion transfer and relieves pressure points with its layered construction, while its two-inch gel foam top helps moderate temperature for cool sleep all night long. A high-density foundation layer provides stable support for the whole mattress. Cradle yourself in the comfort of this memory foam mattress.",
     brand: "Slumber Solutions",
-    rating: 4.8,
+    rating: 5,
     price: 393.99,
     sizes: ["Full", "Queen", "King"],
     imgNum: 3,
@@ -62,7 +62,7 @@ export const AllProducts = [
     description:
       "Once you step onto this ultra plush bath rug, you'll see why it's called Luxury. The Luxury Turkish Cotton bath rug is made with the finest long-staple Turkish fibers to be soft on your toes and durable for the whole family. The thick cotton cut loops and luxe hotel framed design add a touch of elegance to any bathroom. Designed with your safety in mind, this bath rug has a latex anti-skid backing. Pair it with The Welhome Ultra Plush Anderson 6-Piece Towel Set to create a spa-like feel in your own home. ",
     brand: "Welhome",
-    rating: 4.9,
+    rating: 5,
     price: 31.98,
     sizes: ["18x48", "24x60", "20x32"],
     imgNum: 3,
@@ -82,7 +82,7 @@ export const AllProducts = [
     description:
       "Create a bathroom oasis with this Turkish cotton towel set. Made from 100-percent Turkish cotton, each of the six pieces are soft, absorbent, and durable to keep them looking like new, and it's machine washable for easy care. Available in several colors, this set is versatile and blends with your current color scheme.",
     brand: "Authentic Hotel and Spa",
-    rating: 4.3,
+    rating: 4,
     price: 47.99,
     sizes: ["All Included"],
     imgNum: 2,
@@ -102,7 +102,7 @@ export const AllProducts = [
     description:
       "Enjoy ease and great taste in little space with the Keurig K-Slim single serve coffee maker. At just 5-inch wide, the K-Slim single serve coffee maker fits neatly on your countertop, making it the perfect choice for when space-saving and simplicity are important. The K-Slim brewer brings together the performance and simplicity that Keurig is known for, in a slim and sleek design. You also get the convenience you desire with the capacity to brew up to four 8 oz cups before refilling the 46 oz removable water reservoir, and the ability to enjoy a fast and fresh-brewed 8, 10, or 12 oz cup at the push of a button. Choose from hundreds of flavorful K-Cup pod varieties of coffee, tea, cocoa, and more. With the K-Slim brewer, your delicious morning cup or midday pick-me-up is always ready in minutes, and you never have to sacrifice countertop space for taste again. It is compatible with the k-cup universal coffee filter. You can easily brew your own ground coffee (sold separately).",
     brand: "Keurig",
-    rating: 4.1,
+    rating: 4,
     price: 99.0,
     sizes: ["5x7in"],
     imgNum: 2,
@@ -118,7 +118,7 @@ export const AllProducts = [
     description:
       "Complete your kitchen with this modern LG french door refrigerator with 2 adjustable glass shelves. Featuring a silver finish that will complement any interior decor, this refrigerator measures 35.5 inches deep to accommodate any need.",
     brand: "LG",
-    rating: 4.8,
+    rating: 5,
     price: 1609.99,
     sizes: ["21.8-cubic foot"],
     imgNum: 3,
@@ -135,7 +135,7 @@ export const AllProducts = [
     description:
       "Bring out the chef in you with this square frying pan. Featuring dishwasher-safe, PFOA-free granite stone, this triple food-grade pan is ideal for baking, sauteing, searing, or frying. Its natural mineral coating gives it a non-stick finish, while this pan's aluminum body delivers consistent, even heating.",
     brand: "Stoneware",
-    rating: 4.4,
+    rating: 4,
     price: 25.49,
     sizes: ["10", "11", "12"],
     color: ["Black"],
@@ -152,7 +152,7 @@ export const AllProducts = [
     description:
       "Experience Attainable Luxury® with the ZLINE All Gas Professional Range. Combining both professional aesthetics and functionality, this range provides the ultimate luxury experience for a fraction of the cost. Achieve optimal results with cooktop cooking power from 4,200 up to 18,000 BTUs provided with sealed burners. Ignite your culinary experience with an all gas oven, allowing for a faster, more efficient heat and moisture distribution. Enjoy an ultra-deep oven capacity with a three-layered insulated glass oven for even, precise cooking every use. With upgraded premium features such as SmoothSlide ball-bearing oven racks, dual lighting, and adjustable legs, this range is certain to wow both the chef and guests alike. Assembled with the highest quality materials on the market, this range offers a durable, scratch-resistant porcelain cooktop and ZLINE's exclusive single piece cast iron grate. Cook with ease with StayPut Italian hinges, providing a safe baking environment to enjoy for years to come. ZLINE stands by the longevity and durability of their professional ranges, while ensuring further protection and peace of mind with a worry-free warranty. The ZLINE All Gas Range ships next business day when in stock.",
     brand: "Zline Kitchen and Bath",
-    rating: 5.0,
+    rating: 5,
     price: 2282.49,
     sizes: ["30"],
     imgNum: 3,
@@ -169,7 +169,7 @@ export const AllProducts = [
     description:
       "The Conover flatware collection is rendered in weighty stainless steel, and features a classical pattern in the stem. This 65 pieces flatware set includes full service for twelve, including 12 place knives, place forks, place spoons, salad forks and teaspoons; plus one tablespoon, pierced tablespoon, butter knife, sugar shell and cold meat fork.",
     brand: "Waterford",
-    rating: 5.0,
+    rating: 5,
     price: 171.49,
     sizes: ["Service for 12"],
     imgNum: 4,
@@ -187,7 +187,7 @@ export const AllProducts = [
     description:
       "Protect your sofa's upholstery without detracting from the beauty of your space by adding this PrimeBeau quilted sofa-sized slipcover. The reversible slipcover offers two looks in one, and its quilted microfiber construction offers softness and comfort. Toss it in the washer for easy cleaning.",
     brand: "Tsuede",
-    rating: 4.0,
+    rating: 4,
     price: 38.99,
     sizes: ["W75 x L110"],
     imgNum: 1,
@@ -206,7 +206,7 @@ export const AllProducts = [
     description:
       "Crafted from a faux cotton textured fabric, backed with full blackout white liner; soft, thick, durable and elegant. Let their vibrant window treatment be your home artist and add a refreshing element to your room. Developed with high technology blackout feature, these magic window curtains can completely block out 100% sunlight and UV rays and provide an entire dark environment. A great choice for any room.",
     brand: "Waterford",
-    rating: 4.6,
+    rating: 4,
     price: 71.49,
     sizes: ["96in", "108in"],
     imgNum: 3,
@@ -224,7 +224,7 @@ export const AllProducts = [
     description:
       "Old meets new in the Mercurio double gourd table lamp, with its classic shape and sparkling blue glass accented with chrome fittings. This set of two lamps is topped with an elegant drum shade in white cotton for a fashion look in transitional rooms.",
     brand: "afavieh",
-    rating: 5.0,
+    rating: 5,
     price: 133.02,
     sizes: ["15 X 15 X 28.5"],
     imgNum: 3,
@@ -241,7 +241,7 @@ export const AllProducts = [
     description:
       "Offer a space-heating, traditional element to your home with this warm and cozy electric fireplace. The ultra-slim design is fitted with a programmable thermostat, VividFlame LED technology, and adjustable brightness. An included remote control allows for easy adjustment of the flames and heat as you enjoy the lovely look of your chic and inviting space.",
     brand: "Real Flame",
-    rating: 4.8,
+    rating: 5,
     price: 572.99,
     sizes: ["47.4L X 9.5W X 41.9H"],
     imgNum: 3,
@@ -258,7 +258,7 @@ export const AllProducts = [
     description:
       "Lend a touch of softness and comfort to your space with these lovely faux fur throw pillows. The super soft material is fun to touch and rest on, and the gorgeous colors deliver a fun, inviting accent for any space.",
     brand: "Cheer Collection",
-    rating: 4.2,
+    rating: 4,
     price: 35.49,
     sizes: ["18x18", "20x20"],
     imgNum: 1,
@@ -275,7 +275,7 @@ export const AllProducts = [
     description:
       "A marvelous exhibit of trendsetting transitional rugs, the Madison Collection instills life into extraordinary spaces. Expertly power-loomed in Turkey, these rugs are easy-care and virtually non-shedding. Classic designs become fashion-smart home decor in this alluring and playful collection.",
     brand: "Safavieh",
-    rating: 4.7,
+    rating: 5,
     price: 22.12,
     sizes: ["2'3x10'Runner", "2'3x18'Runner", "2'3x22'Runner"],
     imgNum: 2,
@@ -295,7 +295,7 @@ export const AllProducts = [
     description:
       "Combining beautiful floral and vine motifs with impeccable and practical construction, this rug from Safavieh's Florida Shag Collection is a decorator's dream. Lively and graceful, it features a high-low pile sculptural weave for textural dimension. Power-loomed of long-wearing enhanced polypropylene, this collection combines beauty, easy care and outstanding performance.",
     brand: "Safavieh",
-    rating: 4.7,
+    rating: 5,
     price: 22.12,
     sizes: ["2'3x10'Runner", "2'3x8'Runner", "2'3x11'Runner"],
     imgNum: 2,
@@ -315,7 +315,7 @@ export const AllProducts = [
     description:
       "Add a plush and comfortable piece to your home with this white and grey shag rug. This rug is made with extremely comfortable and durable polyester material and will add subtle, yet fashionable appeal to your home.",
     brand: "Nuloom",
-    rating: 4.2,
+    rating: 4,
     price: 173.13,
     sizes: ["4'x6'", "5'x8'"],
     imgNum: 2,
@@ -333,7 +333,7 @@ export const AllProducts = [
     description:
       "Cool grey, taupe, white, and black tones are abundant in the Angie collection. Machine woven with subtle carved details, these rugs are timeless and will look great in any space. Made with a blend of polypropylene and acrylic, these rugs also feel soft underfoot. Produced in Turkey. Spot clean or professionally clean only. Rug pad recommended.",
     brand: "Porch and Den",
-    rating: 4.1,
+    rating: 4,
     price: 92.62,
     sizes: ["6'7'x9'", "5'x8'", "3'x5'"],
     imgNum: 3,
@@ -350,7 +350,7 @@ export const AllProducts = [
     description:
       "The Karaugh collection features a distressed southwestern pattern in cool shades. This collection also offers effortless charm and durability at a great price! Machine woven with 100% polypropylene in Turkey, this stylish synthetic rug is ideal for high-traffic areas, and has minimal shedding. Perfect for a bright cabin. Spot clean or professionally clean only. Rug pad recommended.",
     brand: "Safavieh",
-    rating: 4.6,
+    rating: 5,
     price: 170.99,
     sizes: ["8''x10'", "7'x10'", "6'x9'"],
     imgNum: 3,
@@ -367,7 +367,7 @@ export const AllProducts = [
     description:
       "Give your master bath a vintage-inspired style upgrade with this double vanity sink cabinet. A solid wood frame offers lasting durability, while the three drawers and two cabinets offer storage for towels and personal items. The pre-drilled counter allows for easy faucet installation.",
     brand: "Direct Vanity Sink",
-    rating: 4.7,
+    rating: 5,
     price: 1524.49,
     sizes: ["72"],
     imgNum: 3,
@@ -384,7 +384,7 @@ export const AllProducts = [
     description:
       "Upgrade your bathing experience with this Razor rain shower. The mega-sized head creates a wide spray radius, covering your body in a relaxing flow of water. Adjust the head to suit your frame and cubicle using the extension arm. With its chrome finish, this shower makes a modern statement.",
     brand: "Safavieh",
-    rating: 4.0,
+    rating: 4,
     price: 53.49,
     sizes: ["1'2"],
     imgNum: 3,
@@ -401,7 +401,7 @@ export const AllProducts = [
     description:
       "Upgrade your bathroom with this Vanity Art solid surface freestanding bathtub. Beautiful lines and design will be a perfect addition to any bathroom.",
     brand: "Nuloom",
-    rating: 5.0,
+    rating: 5,
     price: 2259.98,
     sizes: ["59-inch"],
     imgNum: 3,
@@ -418,7 +418,7 @@ export const AllProducts = [
     description:
       "Satisfyingly elegant, the Claire Vessel Sink features a fresh look for classic white ceramic, in true minimalist fashion. Striking simple style, while maximizing precious bathroom space, makes counter independent sinks popular for powder rooms. Features smooth baked-on glaze, and non-porous high-gloss finish, for durable scratch-resistant and easy to clean convenience.",
     brand: "Swiss Madison",
-    rating: 4.9,
+    rating: 5,
     price: 84.49,
     sizes: ["20"],
     imgNum: 3,
@@ -435,7 +435,7 @@ export const AllProducts = [
     description:
       "The single handle kitchen faucet features a professional look with an easy to clean spray face. The pull-down spray head switches from aerated flow to spray at the touch of a button for powerful cleaning. An integrated magnetic catch holds the spray head in place when not in use for added convenience. Protect your family from cold water and hot water, especially elderly people and children.",
     brand: "Kraus",
-    rating: 4.8,
+    rating: 5,
     price: 134.95,
     sizes: [" KPF-3104 - 16 1/4", " KPF-3104 - 16 1/4"],
     imgNum: 4,
