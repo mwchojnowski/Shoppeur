0c504287526ef6f9a00e44cf0d9ae3aeee51f08f
Mateusz Chojnowski <mateusz.w.chojnowski@gmail.com>
2021-02-23 20:08:38 -0500

    Footer

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,12 +1,14 @@
 import React from "react"
 import CheckoutNav from "../components/Checkout/CheckoutNav"
 import CheckoutContent from "../components/Checkout/CheckoutContent"
+import Footer from "../components/Footer"
 
 export default function checkout(){
     return(
         <>
             <CheckoutNav />
             <CheckoutContent />
+            <Footer />
         </>
     )
 }
